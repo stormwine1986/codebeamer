6a5d649fbbb5cce116c3af9513dff4d3fc462032
stormwine1986 <stormwine.jia@gmail.com>
2023-08-02 18:55:22 +0800

    update listener

--- listener.wrapper/listener/attachment_added.groovy
+++ listener.wrapper/listener/attachment_added.groovy
@@ -8,3 +8,31 @@ logger.info("event.request = ${event.request}") // event.request: javax.servlet.
 logger.info("event.data = ${event.data}") // event.data: com.intland.codebeamer.manager.util.ActionData
 logger.info("event.source = ${event.source}") // event.source: com.intland.codebeamer.persistence.util.TrackerItemAttachmentGroup
 logger.info("event.secondarySource = ${event.secondarySource}") // event.secondarySource: [com.intland.codebeamer.persistence.dto.AccessPermissionDto]
+
+logger.info("AttachmentArtifacts: ")
+event.source.attachmentArtifacts.each{
+    it -> 
+    logger.info("it = $it")
+    logger.info("path = ${it.path}")
+    logger.info("map = ${it.attributes}")
+}
+
+logger.info("ActionData: ")
+logger.info("data.attachmentName = ${event.data.attachmentName}")
+logger.info("data.input = ${event.data.input}")
+event.data.uploads.each{
+    it ->
+    logger.info("filename ${it.fileName} file ${it.file}")
+}
+
+/**
+logger.info("HTTP Request: ")
+if(!beforeEvent){
+    event.request.parts.each{
+        it ->
+        logger.info("name ${it.name} size ${it.size} stream ${it.inputStream}")
+    }
+}
+**/
+
+// Attacher
