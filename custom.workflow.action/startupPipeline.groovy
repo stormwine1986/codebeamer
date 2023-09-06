70c577344ea85d6cbb1160d71a06da502add7866
stormwine1986 <stormwine.jia@gmail.com>
2023-09-06 11:13:48 +0800

    update startupPipeline.groovy

--- custom.workflow.action/startupPipeline.groovy
+++ custom.workflow.action/startupPipeline.groovy
@@ -1,6 +1,11 @@
+// startupPipeline
 // codebeamer build task startup a jenkins pipline with some parameters 
 
-if(beforeEvent) return
+if(beforeEvent) {
+  // generate report info link
+  subject.setCustomField(1, "[show|http://alm.cb.test:8080/cb/demo/index.html?taskId=${subject.id}]")
+  return
+}
 
 logger.info("====> startupPipeline.groovy")
 
@@ -14,17 +19,17 @@ import java.lang.String
 import com.intland.codebeamer.event.util.VetoException
 import java.util.Base64
 
-logger.info("bool = ${subject.getCustomField(3)}")
-logger.info("choice = ${subject.getChoiceList(1).get(0).name}")
-logger.info("multiline = ${subject.getCustomField(4)}")
-logger.info("string = ${subject.getCustomField(5)}")
+logger.info("param1 = ${subject.getCustomField(2)}")
+logger.info("param2 = ${subject.getCustomField(3)}")
+logger.info("param3 = ${subject.getChoiceList(0).get(0).name}")
+logger.info("param4 = ${subject.getCustomField(4)}")
 
 // set parameters for pipeline
 def params = new LinkedHashMap()
-params.put("bool",subject.getCustomField(3))
-params.put("choice",subject.getChoiceList(1).get(0).name)
-params.put("multiline",subject.getCustomField(4))
-params.put("string",subject.getCustomField(5))
+params.put("param1",subject.getCustomField(2))
+params.put("param2",subject.getCustomField(3))
+params.put("param3",subject.getChoiceList(0).get(0).name)
+params.put("param4",subject.getCustomField(4))
 params.put("taskId",subject.id)
 
 def postData = new StringBuffer()
@@ -39,11 +44,11 @@ params.each{
 def postDataBytes = postData.toString().getBytes("UTF-8")
 
 // send request to jenkins
-def url = new URL("http://docker-jenkins-1:8080/job/codebeamer/buildWithParameters?token=codebeamer")
+def url = new URL("http://alm.cb.test:8090/job/codebeamer/buildWithParameters?token=codebeamer")
 def conn = (HttpURLConnection) url.openConnection()
 conn.setRequestMethod("POST")
 conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
-conn.setRequestProperty("Authorization", "Basic " + new String(Base64.getEncoder().encode("bond:116c7fdcfa3a65ed91c7a0e1ff46e0c953".getBytes("UTF-8"))))
+conn.setRequestProperty("Authorization", "Basic " + new String(Base64.getEncoder().encode("jenkins:11aaf5e696aafa790c15e5d6dc0efde6c5".getBytes("UTF-8"))))
 conn.setDoOutput(true)
 conn.getOutputStream().write(postDataBytes)
 
