3a51954869129929f3c68afe5fd760ebf52056c5
stormwine1986 <stormwine.jia@gmail.com>
2023-09-07 13:51:17 +0800

    update updateOtherItem.groovy

--- custom.workflow.action/updateOtherItem.groovy
+++ custom.workflow.action/updateOtherItem.groovy
@@ -1,7 +1,10 @@
 // updateOtherItem.groovy
 // update other item with script
 
-if(!beforeEvent) return
+if(beforeEvent) return
+
+// only subject is created, the other item could link the subject as upstream 
+// so this code must execute after event
 
 logger.info("==================> debug start")
 
@@ -13,13 +16,19 @@ import com.intland.codebeamer.persistence.dao.TrackerItemDao
 def trackerItemMgr = applicationContext.getBean(TrackerItemManager.class)
 def trackerItemDao = applicationContext.getBean(TrackerItemDao.class)
 
-def trackerItemDto = trackerItemMgr.findById(user, 1003)
-logger.info("trackerItemDto = $trackerItemDto")
+def sr = trackerItemMgr.findById(user, 1004)
+logger.info("sr = $sr")
 
-def newOne = trackerItemDto.clone() 
+// modify other item
+def delta = sr.clone() 
 def status = new NamedDto()
 status.id = 4
-newOne.setStatus(status)
-trackerItemDao.update(newOne)
+delta.setStatus(status)
+
+// add downstream for subject
+delta.setSubjects([subject])
+
+// save other item
+trackerItemDao.update(delta)
 
 logger.info("===============> debug end")
